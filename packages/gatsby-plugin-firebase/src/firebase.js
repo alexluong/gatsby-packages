7d749fd7f68ae4ab0add7c88348688c423abfa54
Alex Luong <alex@alexluong.com>
2020-03-03 14:25:41 -0800

    fix(gatsby-plugin-firebase): initialize firebase with measurementId

--- packages/gatsby-plugin-firebase/src/firebase.js
+++ packages/gatsby-plugin-firebase/src/firebase.js
@@ -8,7 +8,8 @@ if (!firebase.apps.length) {
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   });
 }
 
